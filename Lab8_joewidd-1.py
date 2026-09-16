98fef0714f2c18ab95aab1ea549c2531bb46f834
Joe Widdifield <joewidd94@gmail.com>
2026-09-16 07:30:14 -0400

    user input is being validated as a number and exactly 12 digits

--- Lab8_joewidd-1.py
+++ Lab8_joewidd-1.py
@@ -5,13 +5,20 @@ a program that validates a 12-digit UPC-A code,
 9/15/2026
 """
 def find_upc():
-    """find and validate upc code"""
+    """find and validate upc-a code"""
+while True:
+    upc = input("Enter a 12-digit UPC: ")
     try:
-        upc = input("Enter a 12-digit UPC: ")
+        if len(upc) != 12:
+            print("Please enter exactly 12 digits")
+            break
         upc_check = int(upc)
         print("the upc entered is " + f"{upc_check}")
+        break
     except ValueError:
-        print("Please enter a valid 12 digit UPC code")
+        print("Please enter a 12 digit number")
+        break
+
 
 
 find_upc()
