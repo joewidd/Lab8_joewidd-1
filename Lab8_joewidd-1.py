94594194df2ab73f89ca7f7b742fd51dae908624
Joe Widdifield <joewidd94@gmail.com>
2026-09-16 07:40:05 -0400

    cleaned up the validation

--- Lab8_joewidd-1.py
+++ Lab8_joewidd-1.py
@@ -8,17 +8,10 @@ def find_upc():
     """find and validate upc-a code"""
 while True:
     upc = input("Enter a 12-digit UPC: ")
-    try:
-        if len(upc) != 12:
-            print("Please enter exactly 12 digits")
-            break
-        upc_check = int(upc)
-        print("the upc entered is " + f"{upc_check}")
-        break
-    except ValueError:
+    if len(upc) != 12 or not upc.isnumeric():
         print("Please enter a 12 digit number")
+    else:
+        print("the upc entered is " + f"{upc}")
         break
-
-
 
 find_upc()
